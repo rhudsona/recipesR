dcfd0e323a0d754b80536ffc88a6acc0ff41b36c
rhudsona <143801145+rhudsona@users.noreply.github.com>
2024-05-27 15:01:21 -0300

    Update GestorPacotes.R
    Inclusão dos comando para Informação de sessão e de pacote

--- GestorPacotes.R
+++ GestorPacotes.R
@@ -2,7 +2,7 @@
 # integrando a funcionalidade das funções básicas para executar várias ações simultaneamente, carregando e se necessário instalando os pacotes ausentes.
 # Os nomes das funções no pacote pacman seguem o formato p_xxx onde 'xxx' é a tarefa que a função executa.
 
-
+p_cran()                                 # Lista pacotes CRAN
 p_install(rpart,MASS)                    # Instala os pacotes rpart e MASS
 p_load(rpart,MASS)                       # Carrega e INSTALA os pacotes rpart e MASS
 p_install_gh("hadley/httr@v0.4")         # Instala pacotes do github.  Os pacotes são passados ​​como endereços de repositório de 
@@ -12,3 +12,12 @@ p_load_gh()                              # Carrega e Instala os pacotes do githu
 p_unload()                               # Descarrega pacotes do caminho de pesquisa, equivalente ao detach()
 p_update()                               # Atualiza pacotes desatualizados
 
+# Informação de sessão
+p_loaded()                              #Lista de pacotes anexados
+p_isloaded()                            #Teste lógico do pacote anexado
+
+#Informações sobre pacotes locais
+p_exists()                              # Verificação lógica se o pacote existe localmente no CRAN
+p_functions()
+p_version()
+p_data()                                #lista com os data.frame fornecidos pelo pacote
